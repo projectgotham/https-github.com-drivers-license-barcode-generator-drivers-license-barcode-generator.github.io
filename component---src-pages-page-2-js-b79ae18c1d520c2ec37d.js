6b0929d413c8ff63c1d389b709df5056a3f3b186
Kyle Decot <kyle@joinroot.com>
2018-11-26 15:38:24 -0500

    Updates

--- component---src-pages-page-2-js-b79ae18c1d520c2ec37d.js
+++ component---src-pages-page-2-js-b79ae18c1d520c2ec37d.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{127:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),c=t(131),l=t(133);n.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-b79ae18c1d520c2ec37d.js.map
+(window.webpackJsonp = window.webpackJsonp || []).push([[5], { 127: function (e, n, t) { 'use strict'; t.r(n); var a = t(0); var o = t.n(a); var c = t(131); var l = t(133); n.default = function () { return o.a.createElement(l.a, null, o.a.createElement('h1', null, 'Hi from the second page'), o.a.createElement('p', null, 'Welcome to page 2'), o.a.createElement(c.Link, { to: '/' }, 'Go back to the homepage')) } } }])
+// # sourceMappingURL=component---src-pages-page-2-js-b79ae18c1d520c2ec37d.js.map
