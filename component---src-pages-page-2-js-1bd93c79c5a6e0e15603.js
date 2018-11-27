a20dfe1ea370c0ab8ea81ff7071cac2bb182e678
Kyle Decot <kyle@joinroot.com>
2018-11-27 08:42:52 -0500

    Updates

---
+++ component---src-pages-page-2-js-1bd93c79c5a6e0e15603.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{128:function(e,n,t){"use strict";t.r(n);var a=t(0),o=t.n(a),c=t(133),l=t(134);n.default=function(){return o.a.createElement(l.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(c.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-1bd93c79c5a6e0e15603.js.map
